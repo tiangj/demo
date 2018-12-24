322c611d371273a6e277d2637a88b6fd2c06de91
tiangj <635636771@qq.com>
2018-12-24 21:31:10 +0800

    Initial commit

---
+++ src/main/java/com/example/contorller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.contorller;
+
+import com.example.entity.User;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by tian on 2018/12/24.
+ */
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("getuser")
+    public User getUser() {
+        User user = new User();
+        user.setName("test");
+        return user;
+    }
+}
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+/**
+ * Created by tian on 2018/12/24.
+ */
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
